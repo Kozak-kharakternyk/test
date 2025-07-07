5a085976956a4e9835d4cf3992abb43635b23144
Ruslan <binary1024x@gmail.com>
2025-07-07 19:06:59 +0300

    Робочі файли

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+
+int main() {
+    std::string name; 
+    
+    std::cout << "Ruslan ";
+    std::cin >> name; 
+    
+    std::cout << "Hello, " << name << "! Welcome to the world of C++!" << std::endl;
+    return 0; 
+}
